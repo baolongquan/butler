7f6f8a02c025718cd078ab38491209b31cb09ef3
baolongquan <14747334145@163.com>
2020-05-17 22:48:46 +0800

    提交框架代码

---
+++ butler-eureka-server/src/main/java/com/butler/ButlerEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButlerEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButlerEurekaServerApplication.class, args);
+    }
+
+}
---
+++ butler-eureka-server/src/test/java/com/butler/ButlerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButlerEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
